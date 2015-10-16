d85bb936f7642dd7a67bd1884a3a02e17fd5d2b5
dgist <dgist@yaerup.org>
2015-10-16 11:04:06 -0400

    10/16/15

---
+++ Practice/src/Starting.java
@@ -0,0 +1,21 @@
+//this is the main class
+public class Starting {
+// this is your main method
+	public static void main(String[] args) {
+		// this says "computer, print this out"
+		System.out.println();
+		int A;
+		double z =7; 
+		
+	
+	}
+public static void  shoeSize(){
+	
+
+}
+public static int blue (int a){
+	int b = a + 7;
+	return b;
+}
+
+}
