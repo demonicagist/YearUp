d87e7bb20f95777ed9fe743af1c3c93601ff91e0
dgist <dgist@yaerup.org>
2015-11-12 14:21:08 -0500

    exceptions lab

---
+++ Exceptions/src/Exceptions.java
@@ -0,0 +1,25 @@
+
+public class Exceptions {
+
+	public static void main(String[] args)
+	{
+		int[] numbers = (1,3,5,7);
+
+	{
+		AddArray(numbers)
+	}
+
+private static void AddArray(int [] numbers)
+{
+	int sum = 0;
+	
+	int i;
+	for (int = 0; i <=numbers.length; i++)
+
+	
+		
+	}
+	System.out.println("Sum is: ")
+	}
+
+}}}
