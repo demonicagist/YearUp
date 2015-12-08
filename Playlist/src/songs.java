da48c4dc3a265f1f333e59cbe5cf3af9275a20fa
dgist <dgist@yaerup.org>
2015-12-08 11:27:04 -0500

    12/08/2015

---
+++ Playlist/src/songs.java
@@ -0,0 +1,11 @@
+
+public class songs {
+
+	public static void main(String[] args) {
+		System.out.println("The Name of the song is Love by Alicia Keys");
+		System.out.println("I hate Mondays by the Stones");
+		System.out.println("Star spangled banner");
+
+	}
+
+}
